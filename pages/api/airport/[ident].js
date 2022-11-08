8f1e3bd073824949083cc4b86e49149997c13188
alpineux <99145815+alpineux@users.noreply.github.com>
2022-11-08 13:45:52 -0700

    Update [ident].js

--- pages/api/airport/[ident].js
+++ pages/api/airport/[ident].js
@@ -5,19 +5,26 @@ export default async function handler(req, res) {
 
 	const { query } = req
 
-	if(!query.ident) res.status(400).json({ error: 'No airport ICAO or IATA provided', code: 'NAII452' })
-	if(query.ident.length !== 4 && query.ident.length !== 3) res.status(400).json({ error: 'Invalid ICAO or IATA', code: 'III456' })
+	if(!query.ident) {
+		res.status(400).json({ error: 'No airport ICAO or IATA provided', code: 'NAII452' })
+		return;
+	}
+
+	if(query.ident.length !== 4 && query.ident.length !== 3) {
+		res.status(400).json({ error: 'Invalid ICAO or IATA', code: 'III456' })
+		return;
+	}
 
 	const fetch = await single({ code: query.ident.toUpperCase() })
 
 	if(fetch.error) { 
 		res.status(400).json({ error: 'There was an error fetching the data', code: 'EFD998' }) 
-		return 
+		return;
 	}
 
 	if(fetch.empty) { 
 		res.status(400).json({ error: 'No results found', code: 'NRF492' }) 
-		return 
+		return;
 	}
 
 	res.status(200).json(fetch.data)
