48d49b43b435b1c71647d8dcba5df0dc138db861
alpineux <99145815+alpineux@users.noreply.github.com>
2022-11-08 13:36:02 -0700

    Update [ident].js

--- pages/api/airport/[ident].js
+++ pages/api/airport/[ident].js
@@ -10,8 +10,15 @@ export default async function handler(req, res) {
 
 	const fetch = await single({ code: query.ident.toUpperCase() })
 
-	if(fetch.error) { res.status(400).json({ error: 'There was an error fetching the data', code: 'EFD998' }) return; }
-	if(fetch.empty) { res.status(400).json({ error: 'No results found', code: 'NRF492' }) return; }
+	if(fetch.error) { 
+		res.status(400).json({ error: 'There was an error fetching the data', code: 'EFD998' }) 
+		return 
+	}
+
+	if(fetch.empty) { 
+		res.status(400).json({ error: 'No results found', code: 'NRF492' }) 
+		return 
+	}
 
 	res.status(200).json(fetch.data)
 }
